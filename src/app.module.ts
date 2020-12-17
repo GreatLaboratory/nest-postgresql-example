d7b3f37de0c3c4870549eb53451f15439fe96a7d
Myung-gwan Kim <cartopia95@naver.com>
2020-12-17 23:33:34 +0900

    add - db connection & global config

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DATABASE_HOST,
+            port: 5432,
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            autoLoadEntities: true,
+            synchronize: true,
+            entities: ['dist/**/*.entity{.ts,.js}'],
+        }),
+        UserModule, 
+    ],
+    controllers: [],
+    providers: [],
+})
+export class AppModule {}

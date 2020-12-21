fe0a75c35c00ea818404b748af558872dc93b123
Myung-gwan Kim <cartopia95@naver.com>
2020-12-22 00:11:59 +0900

    add - swagger문서

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap () {
     const app = await NestFactory.create(AppModule);
+    const options = new DocumentBuilder()
+        .setTitle('NestJS PostgreSQL example')
+        .setDescription('API description')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
     app.use(morgan('tiny'));
     app.useGlobalPipes(new ValidationPipe({
         whitelist: true,

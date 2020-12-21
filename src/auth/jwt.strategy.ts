362d2234aa5f4e3ece0369762598898f86420ecb
Myung-gwan Kim <cartopia95@naver.com>
2020-12-21 15:46:28 +0900

    feat - jwt passport 설정

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor () {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.JWT_SECRET,
+        });
+    }
+
+    async validate (payload: any) {
+        // payload는 user객체, jwt발급날짜시간, jwt만료날짜시간 으로 구성
+        return payload.user;
+    }
+}
